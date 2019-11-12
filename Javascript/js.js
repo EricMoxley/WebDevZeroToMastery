0c46709dbcd02fedea7d8c690f40f2c5b3fbebf1
EricMoxley <exilerzero@yahoo.com>
2019-11-12 17:19:24 -0500

    updates continuing course

---
+++ Javascript/js.js
@@ -0,0 +1,8 @@
+var name = "Bill";
+if (name === "Bill" || name = "Anne") {
+    alert("hi Bill or Anne!");
+} else if (name === "Sue") {
+    alert("hi Sue!");
+} else {
+    alert("I don't know you.");
+}
