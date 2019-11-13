d1e286061ca6e25f408c554c3f0db33817fc334a
EricMoxley <exilerzero@yahoo.com>
2019-11-13 10:45:14 -0500

    update

---
+++ JS2/objects.js
@@ -0,0 +1,21 @@
+var user = {
+    name: "John",
+    age: 34,
+    hobby: "Soccer",
+    isMarried: false,
+    shout: function() {
+        console.log("AHHH");
+    }
+};
+//^ call in console by user.name, user.age, user.hobby, etc
+//^ can change items in objects by e.g. user.isMarried = true , etc.
+
+
+var list = ["apple", "banana", "orange"];
+
+var list = [
+    {
+        username: "andy",
+        password: "secret",
+    }
+];
---
+++ JS2/script.js
@@ -0,0 +1,20 @@
+//arrays examples
+
+var list = ["tiger", "cat", "bear", "bird"]; 
+console.log(list[0]);
+           //^ Gets "tiger"
+
+           //^ list gets full list in browser console
+
+list.shift();
+"tiger"
+//^removes tiger, shifts list to left
+list.pop();
+"bird"
+//^removes bird
+list.push("elephant");
+//^adds elephant
+list.concat(["bee", "deer"]);
+//^adds bee and deer to end of list
+list.sort();
+//^sorted list by alphabetical order (sorts original list until new adds are assigned to a var)
