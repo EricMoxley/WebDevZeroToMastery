ee7ccb7e54bd8fd05122a9ae8939b85dc3e1bd49
EricMoxley <exilerzero@yahoo.com>
2019-11-18 18:40:22 -0500

    update

---
+++ JSLoops/script.js
@@ -0,0 +1,12 @@
+const todos = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript",
+    "eat healthy"
+];
+
+for (const i=0; i < todos.length; i++) {
+    todos.pop();
+}
+
--- Project7_FB/script.js
+++ Project7_FB/script.js
@@ -1,11 +1,19 @@
-const database = [
+var database = [
     {
         username: "eric",
         password: "secret"
+    },
+    {
+        username: "vincent",
+        password: "123"
+    },
+    {
+        username: "moxley",
+        password: "777"
     }
 ];
 
-const newsFeed = [
+var newsFeed = [
     {
         username: "Bob",
         timeline: "So tired from all that learning."
@@ -13,19 +21,33 @@ const newsFeed = [
     {
         username: "Sally",
         timeline: "Javascript is so cool."
+    },
+    {
+        username: "George",
+        timeline: "Python is better!"
     }
 ];
 
-const userNamePrompt = prompt("What's your username?");
-const passwordPrompt = prompt("What's your password?");
 
-function signIn(user, pass) {
-    if (user === database[0].username && 
-        pass === database[0].password){
+function isUserValid(username, password) {
+    for (const i=0; i < database.length; i++) {
+        if (database[i].username === username &&
+            database[i].password === password) {
+                return true;
+            }
+    }
+    return false;
+}
+
+function signIn(username, password) {
+    if (isUserValid(username, password)) {
         console.log(newsFeed);
     } else {
         alert("Sorry, wrong username and password!");
     }
 }
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
 
 signIn(userNamePrompt, passwordPrompt);
