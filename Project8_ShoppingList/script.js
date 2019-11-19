38f442b43e6f057fedfa8fdf116111ce19546238
EricMoxley <exilerzero@yahoo.com>
2019-11-19 07:36:42 -0500

    callback functions

---
+++ Project8_ShoppingList/script.js
@@ -0,0 +1,32 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+//^Cache query
+function inputLength() {
+    return input.value.length;
+};
+
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);    
+    input.value = "";
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
+    };
+};
+
+function addListAfterKeypress(event){
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    };
+};
+//^functions created for create list to add to child, add to list after a click, add to list after keypress code 13 (enter)
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+
+//^button and input listener actions, then function it calls back (callback functions) for that we created above.
